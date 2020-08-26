6219197384ae55a9f646e5aa482f96dd0e529ee6
Tsedtopia <68248967+Tsedtopia@users.noreply.github.com>
2020-08-26 18:19:50 -0500

    Zinab commit message

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Deliverable 3. Create a Travel Itinerary Map.
+
+# In[3]:
+
+
+# Dependencies and Setup
+import pandas as pd
+
+
+# Import API key
+weather_api_key="3c1b65be65c2737e746437ba1260c1e7"
+#g_key =
